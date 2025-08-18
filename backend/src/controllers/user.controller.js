5341e7ceb9ebfd370cda376144e51c139539113b
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-18 13:51:26 +0900

    create a FriendRequest model; create a controller that sends friend request;

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../models/FriendRequest.js";
 import User from "../models/User.js";
 
 export const getRecommendedUsers = async (req, res) => {
@@ -36,4 +37,47 @@ export const getMyFriends = async (req, res) => {
   }
 };
 
-export const sendFriendRequest = async (req, res) => {};
+export const sendFriendRequest = async (req, res) => {
+  try {
+    const myId = req.user.id;
+    const { id: recipientId } = req.params;
+
+    if (myId === recipientId)
+      return res
+        .status(400)
+        .json({ message: "You cannot send a friend request to yourself" });
+
+    const recipient = await User.findById(recipientId);
+
+    if (!recipient) {
+      return res.status(404).json({ message: "Recipient not found" });
+    }
+
+    if (recipient.friends.includes(myId)) {
+      return res
+        .status(400)
+        .json({ message: "You are already friends with this user" });
+    }
+
+    const existingRequest = await FriendRequest.findOne({
+      $or: [
+        { sender: myId, recipient: recipientId },
+        { sender: recipientId, recipient: myId },
+      ],
+    });
+
+    if (existingRequest) {
+      return res.status(400).json({ message: "Friend request already sent" });
+    }
+
+    const friendRequest = await FriendRequest.create({
+      sender: myId,
+      recipient: recipientId,
+    });
+
+    res.status(201).json(friendRequest);
+  } catch (error) {
+    console.log("Error in sending friend request in user controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
