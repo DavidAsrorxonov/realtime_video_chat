b4ef7f1f4ff4f02065db51607fb812e85a997fe5
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-18 23:52:22 +0900

    fix routing logic;

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,10 @@ import PageLoader from "./components/PageLoader";
 import useAuthUser from "./hooks/useAuthUser";
 
 const App = () => {
-  const { isLoading, authUser } = useAuthUser;
+  const { isLoading, authUser } = useAuthUser();
+
+  const isAuthenticated = Boolean(authUser);
+  const isOnboarded = authUser?.isOnboarded;
 
   if (isLoading) return <PageLoader />;
 
@@ -22,32 +25,40 @@ const App = () => {
       <Routes>
         <Route
           path="/"
-          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+          element={
+            isAuthenticated && isOnboarded ? (
+              <HomePage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
         />
         <Route
           path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+          element={!isAuthenticated ? <SignUpPage /> : <Navigate to={"/"} />}
         />
         <Route
           path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+          element={!isAuthenticated ? <LoginPage /> : <Navigate to={"/"} />}
         />
         <Route
           path="/onboarding"
-          element={authUser ? <OnboardingPage /> : <Navigate to={"/login"} />}
+          element={
+            isAuthenticated ? <OnboardingPage /> : <Navigate to={"/login"} />
+          }
         />
         <Route
           path="/call"
-          element={authUser ? <CallPage /> : <Navigate to={"/login"} />}
+          element={isAuthenticated ? <CallPage /> : <Navigate to={"/login"} />}
         />
         <Route
           path="/chat"
-          element={authUser ? <ChatPage /> : <Navigate to={"/login"} />}
+          element={isAuthenticated ? <ChatPage /> : <Navigate to={"/login"} />}
         />
         <Route
           path="/notifications"
           element={
-            authUser ? <NotificationsPage /> : <Navigate to={"/login"} />
+            isAuthenticated ? <NotificationsPage /> : <Navigate to={"/login"} />
           }
         />
       </Routes>
