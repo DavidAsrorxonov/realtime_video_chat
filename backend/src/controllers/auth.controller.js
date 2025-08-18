554fa587f108bcb5523df0babf7657b1908f5b64
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-18 12:25:32 +0900

    implement login and logout logic in controllers; create a password matching function in the user schema;

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -63,9 +63,46 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login");
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+    const isPasswordCorrect = await user.comparePassword(password);
+
+    if (!isPasswordCorrect)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({
+      success: true,
+      user,
+      message: "User logged in successfully",
+    });
+  } catch (error) {
+    console.log("Error in login controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout");
+  res.clearCookie("jwt");
+
+  res.status(200).json({ message: "User logged out successfully" });
 };
