ca3088599517d8f4676d6db21af466c11cd3844f
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-18 14:37:24 +0900

    add exports for pages and use them in App.jsx;

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Routes } from "react-router";
+import { Route, Routes } from "react-router";
+import {
+  HomePage,
+  SignUpPage,
+  LoginPage,
+  OnboardingPage,
+  CallPage,
+  ChatPage,
+  NotificationsPage,
+} from "./pages";
 
 const App = () => {
   return (
     <div className="h-screen" data-theme="night">
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+      </Routes>
     </div>
   );
 };
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,17 @@
+import HomePage from "./HomePage";
+import SignUpPage from "./SignUpPage";
+import LoginPage from "./LoginPage";
+import NotificationsPage from "./NotificationsPage";
+import CallPage from "./CallPage";
+import ChatPage from "./ChatPage";
+import OnboardingPage from "./OnboardingPage";
+
+export {
+  HomePage,
+  SignUpPage,
+  LoginPage,
+  NotificationsPage,
+  CallPage,
+  ChatPage,
+  OnboardingPage,
+};
