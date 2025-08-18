a5113f5f230601a5950d348769d9e359a7cb7da4
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-18 13:17:18 +0900

    implement the logic for getting recommended users and my friends functions;

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,37 @@
-export const getRecommendedUsers = async (req, res) => {};
+import User from "../models/User.js";
 
-export const getMyFriends = async (req, res) => {};
+export const getRecommendedUsers = async (req, res) => {
+  try {
+    const currentUserId = req.user.id;
+    const currectUser = req.user;
+
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } },
+        { _id: { $nin: currectUser.friends } },
+        { isOnboarded: true },
+      ],
+    });
+
+    res.status(200).json(recommendedUsers);
+  } catch (error) {
+    console.log("Error in getting recommended users in user controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getMyFriends = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id)
+      .select("friends")
+      .populate(
+        "friends",
+        "fullName profilePic nativeLanguage learningLanguage"
+      );
+
+    res.status(200).json(user.friends);
+  } catch (error) {
+    console.log("Error in getting my friends in user controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
