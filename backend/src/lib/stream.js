31b34339c434977f0ef4ea575b7cbd848ca8fdac
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-18 12:31:39 +0900

    create a stream configuration and upserting function to update or create a user in the stream platform;

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,24 @@
+import { StreamChat } from "stream-chat";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+  console.error("Missing Stream API key or secret");
+}
+
+const streamClient = new StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUsers([userData]);
+    return userData;
+  } catch (error) {
+    console.log("Error upserting user in Stream", error);
+  }
+};
+
+export const generateStreamToken = async (userId) => {};
