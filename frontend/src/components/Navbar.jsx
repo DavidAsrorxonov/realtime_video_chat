12ed963e5bd74fb23a3848502f101b44781e80bb
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-19 13:53:52 +0900

    add more checkups for the logout function as well; navbar component completed;

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import useAuthUser from "../hooks/useAuthUser";
+import { Link, useLocation } from "react-router";
+import { useQueryClient } from "@tanstack/react-query";
+import useAuth from "../hooks/useAuth";
+import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { authUser } = useAuthUser();
+  const location = useLocation();
+  const isChatPage = location.pathname?.startsWith("/chat");
+
+  const { mutate } = useAuth("logout");
+
+  return (
+    <div className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-end w-full">
+          {isChatPage && (
+            <div className="pl-5">
+              <Link to="/" className="flex items-center gap-2.5">
+                <ShipWheelIcon className="size-9 text-primary" />
+                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
+                  Streamify
+                </span>
+              </Link>
+            </div>
+          )}
+
+          <div className="flex items-center gap-3 sm:gap-4 ml-auto">
+            <Link to={"/notifications"}>
+              <button className="btn btn-ghost btn-circle">
+                <BellIcon className="h-6 w-6 text-base-content opacity-70" />
+              </button>
+            </Link>
+          </div>
+
+          <div className="avatar">
+            <div className="w-9 rounded-full">
+              <img
+                src={authUser?.profilePic}
+                alt="User Avatar"
+                rel="noreferrer"
+              />
+            </div>
+          </div>
+
+          <button className="btn btn-ghost btn-circle" onClick={mutate}>
+            <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,16 +1,27 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import React from "react";
-import { login, signup } from "../lib/api";
+import { login, logout, signup } from "../lib/api";
 
 const useAuth = (type) => {
   const queryClient = useQueryClient();
 
   const { mutate, error, isPending } = useMutation({
-    mutationFn: type === "login" ? login : type === "signup" ? signup : null,
+    mutationFn:
+      type === "login"
+        ? login
+        : type === "signup"
+        ? signup
+        : type === "logout"
+        ? logout
+        : null,
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
   });
 
-  return { mutate, error, isPending };
+  if (type === "login" || type === "signup") {
+    return { mutate, error, isPending };
+  }
+
+  return { mutate };
 };
 
 export default useAuth;
