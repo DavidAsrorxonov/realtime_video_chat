a53fef05dafff610fbb5c49b2f338907517ba4b9
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-19 12:55:09 +0900

    comlete the login page; add login function; modify the routes;

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -6,6 +6,12 @@ export const signup = async (data) => {
   return res.data;
 };
 
+export const login = async (data) => {
+  const res = await axiosInstance.post("/auth/login", data);
+
+  return res.data;
+};
+
 export const getAuthUser = async () => {
   const res = await axiosInstance.get("/auth/me");
   return res.data;
